968e3a24eb0c3409dc84e8d0b18acfcd8af496c4
mathieu <mathvdt@DESKTOP-FDNHF2C.localdomain>
2021-06-14 21:17:11 +0200

    Ajout du make file et correction .h avec les includes et define cst

--- animation_simple/animation_simple.h
+++ animation_simple/animation_simple.h
@@ -1,6 +1,13 @@
 #ifndef __ANIMATION_SIMPLE__
 #define __ANIMATION_SIMPLE__
 
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+#define LARGEUR_FENETRE 1000
+#define HAUTEUR_FENETRE 800
+
+
 void end_sdl(char ok, char const *msg, SDL_Window *window, SDL_Renderer *renderer);
 void draw(SDL_Renderer *renderer, int x_pos, int y_pos);
 void affichage_perso(SDL_Renderer *renderer, int x_pos, int y_pos);
