68ac60b784af13617555bcd40d2407910fab6a6b
mathieu <ma_th21@hotmail.fr>
2021-06-15 18:15:28 +0200

    ajout fct affichage bg et sprite

--- animation_texture/mathieu/main.c
+++ animation_texture/mathieu/main.c
@@ -38,6 +38,7 @@ int main()
     SDL_Texture *trou_noir;
     SDL_Texture *sprite;
     SDL_Texture *sun;
+    SDL_Texture *shadow;
 
     background = IMG_LoadTexture(renderer, IMG_BACKGROUND);
     if (background == NULL)
@@ -51,12 +52,17 @@ int main()
     sun = IMG_LoadTexture(renderer, IMG_SUN);
     if (sun == NULL)
         end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer);
+    shadow = IMG_LoadTexture(renderer, IMG_SHADOW);
+    if (sun == NULL)
+        end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer);
 
     play_texture_full_window(background, window, renderer);
     play_texture_xy(trou_noir, window, renderer);
     SDL_RenderPresent(renderer); // affichage
     SDL_Delay(1000);
-    play_texture_sprite_xy(sun, window, renderer);
+    play_texture_anime(sun, window, renderer);
+    play_texture_sprite_courir(sprite, window, renderer);
+    play_texture_sprite_avec_bg(background, shadow, window, renderer);
 
     SDL_Delay(2000);
     SDL_RenderClear(renderer);
@@ -70,12 +76,15 @@ void draw(SDL_Renderer *renderer, int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int
 {
     int i, j, c;
     SDL_Rect rect;
+    int opacite = 255;
     for (i = 0; i < HAUTEUR_MATRICE; i++)
     {
         for (j = 0; j < LARGEUR_MATRICE; j++)
         {
             c = map[i][j];
-            SDL_SetRenderDrawColor(renderer, couleurs[c][0], couleurs[c][1], couleurs[c][2], 255);
+            if (c == 0)
+                opacite = 0;
+            SDL_SetRenderDrawColor(renderer, couleurs[c][0], couleurs[c][1], couleurs[c][2], opacite);
             rect.x = j * TAILLE_PIXEL;
             rect.y = i * TAILLE_PIXEL;
             rect.w = 1 * TAILLE_PIXEL;
@@ -186,7 +195,7 @@ void play_texture_xy(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *
     SDL_RenderCopy(renderer, my_texture, &source, &destination);
 }
 
-void play_texture_sprite_xy(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer)
+void play_texture_anime(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer)
 {
     SDL_Rect
         source = {0},            // Rectangle définissant la zone de la texture à récupérer
@@ -221,4 +230,124 @@ void play_texture_sprite_xy(SDL_Texture *my_texture, SDL_Window *window, SDL_Ren
         SDL_Delay(30);                                                     // Pause en ms
     }
     SDL_RenderClear(renderer);
+}
+
+void play_texture_sprite_courir(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer)
+{
+    SDL_Rect
+        source = {0},            // Rectangle définissant la zone totale de la planche
+        window_dimensions = {0}, // Rectangle définissant la fenêtre, on n'utilisera que largeur et hauteur
+        destination = {0},       // Rectangle définissant où la zone_source doit être déposée dans le renderer
+        state = {0};             // Rectangle de la vignette en cours dans la planche
+
+    SDL_GetWindowSize(window, // Récupération des dimensions de la fenêtre
+                      &window_dimensions.w,
+                      &window_dimensions.h);
+    SDL_QueryTexture(my_texture, // Récupération des dimensions de l'image
+                     NULL, NULL,
+                     &source.w, &source.h);
+
+    /* Mais pourquoi prendre la totalité de l'image, on peut n'en afficher qu'un morceau, et changer de morceau :-) */
+
+    int nb_images = 8;                   // Il y a 8 vignette dans la ligne de l'image qui nous intéresse
+    float zoom = 2;                      // zoom, car ces images sont un peu petites
+    int offset_x = source.w / nb_images, // La largeur d'une vignette de l'image, marche car la planche est bien réglée
+        offset_y = source.h / 4;         // La hauteur d'une vignette de l'image, marche car la planche est bien réglée
+
+    state.x = 0;            // La première vignette est en début de ligne
+    state.y = 3 * offset_y; // On s'intéresse à la 4ème ligne, le bonhomme qui court
+    state.w = offset_x;     // Largeur de la vignette
+    state.h = offset_y;     // Hauteur de la vignette
+
+    destination.w = offset_x * zoom; // Largeur du sprite à l'écran
+    destination.h = offset_y * zoom; // Hauteur du sprite à l'écran
+
+    destination.y = // La course se fait en milieu d'écran (en vertical)
+        (window_dimensions.h - destination.h) / 2;
+
+    int speed = 9;
+    for (int x = 0; x < window_dimensions.w - destination.w; x += speed)
+    {
+        destination.x = x;   // Position en x pour l'affichage du sprite
+        state.x += offset_x; // On passe à la vignette suivante dans l'image
+        state.x %= source.w; // La vignette qui suit celle de fin de ligne est
+                             // celle de début de ligne
+
+        SDL_RenderClear(renderer);           // Effacer l'image précédente avant de dessiner la nouvelle
+        SDL_RenderCopy(renderer, my_texture, // Préparation de l'affichage
+                       &state,
+                       &destination);
+        SDL_RenderPresent(renderer); // Affichage
+        SDL_Delay(80);               // Pause en ms
+    }
+    SDL_RenderClear(renderer); // Effacer la fenêtre avant de rendre la main
+}
+
+void play_texture_sprite_avec_bg(SDL_Texture *bg_texture, SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer)
+{
+    SDL_Rect
+        source = {0},            // Rectangle définissant la zone de la texture à récupérer
+        window_dimensions = {0}, // Rectangle définissant la fenêtre, on  n'utilisera que largeur et hauteur
+        destination = {0};       // Rectangle définissant où la zone_source doit être déposée dans le renderer
+
+    SDL_GetWindowSize(window, // Récupération des dimensions de la fenêtre
+                      &window_dimensions.w,
+                      &window_dimensions.h);
+    SDL_QueryTexture(my_texture, NULL, NULL, // Récupération des dimensions de l'image
+                     &source.w, &source.h);
+
+    int nb_images = 40;                      //  Il y a 8 vignette dans la ligne qui nous intéresse
+    int nb_images_animation = 1 * nb_images; //
+    float zoom = 2;                          // zoom, car ces images sont un peu petites
+    int offset_x = source.w / 4,             // La largeur d'une vignette de l'image
+        offset_y = source.h / 5;             // La hauteur d'une vignette de l'image
+    SDL_Rect state[40];                      // Tableau qui stocke les vignettes dans le bon ordre pour l'animation
+
+    /* construction des différents rectangles autour de chacune des vignettes de la planche */
+    int i = 0;
+    for (int y = 0; y < source.h; y += offset_y)
+    {
+        for (int x = 0; x < source.w; x += offset_x)
+        {
+            state[i].x = x;
+            state[i].y = y;
+            state[i].w = offset_x;
+            state[i].h = offset_y;
+            ++i;
+        }
+    }
+    state[14] = state[13]; // state[14 à 16] la même image, le monstre ne bouge pas
+    state[15] = state[16];
+    for (; i < nb_images; ++i)
+    { // reprise du début de l'animation en sens inverse
+        state[i] = state[39 - i];
+    }
+
+    destination.w = offset_x * zoom;            // Largeur du sprite à l'écran
+    destination.h = offset_y * zoom;            // Hauteur du sprite à l'écran
+    destination.x = window_dimensions.w * 0.75; // Position en x pour l'affichage du sprite
+    destination.y = window_dimensions.h * 0.7;  // Position en y pour l'affichage du sprite
+
+    i = 0;
+    for (int cpt = 0; cpt < nb_images_animation; ++cpt)
+    {
+        play_texture_full_window(bg_texture, // identique à play_with_texture_1, où on a enlevé l'affichage et la pause
+                                 window, renderer);
+        SDL_RenderCopy(renderer, // Préparation de l'affichage
+                       my_texture, &state[i], &destination);
+        i = (i + 1) % nb_images;     // Passage à l'image suivante, le modulo car l'animation est cyclique
+        SDL_RenderPresent(renderer); // Affichage
+        SDL_Delay(100);              // Pause en ms
+    }
+    SDL_RenderClear(renderer); // Effacer la fenêtre avant de rendre la main
+}
+
+void animation(SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *bg_texture, SDL_Texture *trou_noir, SDL_Texture *sprite, SDL_Texture *shadow, int map[HAUTEUR_MATRICE][LARGEUR_MATRICE])
+{
+    int x_perso = 70;
+    int y_perso = 10;
+
+    
+
+
 }
--- animation_texture/mathieu/main.h
+++ animation_texture/mathieu/main.h
@@ -6,13 +6,14 @@
 #define IMG_TROU_NOIR "/home/local.isima.fr/mavilledie4/shared/projetZ1/exemples_executables/img/trou-noir.png"
 #define IMG_SPRITE "/home/local.isima.fr/mavilledie4/shared/projetZ1/exemples_executables/img/player-spritemap-v9.png"
 #define IMG_SUN "/home/local.isima.fr/mavilledie4/shared/projetZ1/exemples_executables/img/sun.png"
+#define IMG_SHADOW "/home/local.isima.fr/mavilledie4/shared/projetZ1/exemples_executables/img/shadow-80x70.png"
 
 #define LARGEUR_FENETRE 1000
 #define HAUTEUR_FENETRE 800
 #define NOM_FENETRE "Fenêtre"
 
 #define LARGEUR_MATRICE 100
-#define HAUTEUR_MATRICE 100
+#define HAUTEUR_MATRICE 80
 
 #define TAILLE_PIXEL 10
 
@@ -24,6 +25,8 @@ int dessine_perso(int i, int j);
 
 void play_texture_full_window(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
 void play_texture_xy(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
-void play_texture_sprite_xy(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
-
+void play_texture_anime(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
+void play_texture_sprite_courir(SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
+void play_texture_sprite_avec_bg(SDL_Texture *bg_texture, SDL_Texture *my_texture, SDL_Window *window, SDL_Renderer *renderer);
+void animation(SDL_Window *window, SDL_Renderer *renderer, SDL_Texture *bg_texture, SDL_Texture *trou_noir, SDL_Texture *sprite, SDL_Texture *shadow, int map[HAUTEUR_MATRICE][LARGEUR_MATRICE]);
 #endif
