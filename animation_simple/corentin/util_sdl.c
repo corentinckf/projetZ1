528cbb23d53874397b5d1c77f75fc1a8b1da583b
corentin <pcchienkan@a112pc19L.local.isima.fr>
2021-06-15 12:03:08 +0200

    anim

---
+++ animation_simple/corentin/util_sdl.c
@@ -0,0 +1,67 @@
+#include "util_sdl.h"
+
+SDL_Window * initWindow(int width, int height,int x_pos, int y_pos, char * window_name)
+{
+    SDL_Window * window;
+
+    if (SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("Error : SDL initialisation - %s\n", SDL_GetError()); // l'initialisation de la SDL a échoué
+        exit(EXIT_FAILURE);
+    }
+
+    window = SDL_CreateWindow(
+        window_name,                       // window title
+        x_pos,                             // initial x position
+        y_pos,                             // initial y position
+        width,                               // width, in pixels
+        height,                               // height, in pixels
+        SDL_WINDOW_OPENGL
+    );
+
+    if (window == NULL) {
+        printf("Error : Window initialisation - %s\n", SDL_GetError());
+        window = NULL;
+    }
+
+    return window;
+
+}
+
+void end_sdl(char ok,                                                 // fin normale : ok = 0 ; anormale ok = 1
+                  char const* msg,                                    // message à afficher
+                  SDL_Window* window,                                 // fenêtre à fermer
+                  SDL_Renderer* renderer,
+                  int * code_retour) {                           // renderer à fermer
+  char msg_formated[255];                                         
+  int l;                                                          
+
+  if (!ok) {                                                      
+         strncpy(msg_formated, msg, 250);                                 
+         l = strlen(msg_formated);                                        
+         strcpy(msg_formated + l, " : %s\n");                     
+
+         SDL_Log(msg_formated, SDL_GetError());                   
+  }                                                               
+
+  if (renderer != NULL) SDL_DestroyRenderer(renderer);                            
+  if (window != NULL)   SDL_DestroyWindow(window);                                        
+
+  SDL_Quit();                                                     
+
+  if (!ok) {
+      *code_retour = -1;  //exit(EXIT_FAILURE)  après                                                                                             
+  }                                                               
+}  
+
+int test_fenetre(SDL_Window *fenetre)
+{
+    int code_retour = 1;
+    if (fenetre == NULL)
+    {
+        SDL_Log("Error : SDL Window creation - %s\n", SDL_GetError()); // échec de la création de la fenêtre
+        SDL_Quit();
+        code_retour = 0;
+    }
+    return code_retour;
+}
