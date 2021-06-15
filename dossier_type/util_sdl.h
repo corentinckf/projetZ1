8b13eb998eec1376402927ec3cfb55a1bd9cd8f6
corentin <pcchienkan@a112pc19L.local.isima.fr>
2021-06-15 09:55:41 +0200

    include lib added

--- dossier_type/util_sdl.h
+++ dossier_type/util_sdl.h
@@ -4,6 +4,8 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 SDL_Window * initWindow(int , int, int, int, char *);
 void end_sdl(char,char const *, SDL_Window*, SDL_Renderer*, int *);
