96d4cd061de1d3fb3f73ba955fefd68e7e9a49bb
anthirion <antoine.thirion@etu.uca.fr>
2021-06-15 16:57:27 +0200

    ajout des fichiers .h

---
+++ serpents.h
@@ -0,0 +1,7 @@
+#ifndef __serpents_h__
+#define __serpents_h__
+
+void end_sdl(char ok,char const* msg,SDL_Window* window,SDL_Renderer* renderer);
+void draw(SDL_Renderer* renderer, int *x, int *nb);
+
+#endif
