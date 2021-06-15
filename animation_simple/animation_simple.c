9936f0ff0da0ef8ec873bbfe28d1df5efd7317fd
mathieu <ma_th21@hotmail.fr>
2021-06-15 10:26:01 +0200

    Animation matrice fonctionnel, perso bouge

--- animation_simple/animation_simple.c
+++ animation_simple/animation_simple.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-    int x_perso = 50;
+    int i;
+    int x_perso = 10;
     int y_perso = 80;
     int map[HAUTEUR_MATRICE][LARGEUR_MATRICE];
     int couleurs[6][3] = {{135, 206, 235},
@@ -10,7 +11,7 @@ int main()
                           {0, 0, 255},
                           {245, 245, 220},
                           {0, 0, 0},
-                          {250, 250, 210}};
+                          {250, 250, 0}};
 
     //ciel 0    //tete 3
     //jambe 1   //cheveux 4
@@ -47,22 +48,127 @@ int main()
     if (renderer == NULL)
         end_sdl(0, "ERROR RENDERER CREATION", fenetre, renderer);
 
-    init_map(map);
-
-    placement_perso(map, x_perso, y_perso);
-
     /**************On dessine dans le renderer *************/
-    draw(renderer, map, couleurs); // appel de la fonction qui crée l'image
-    SDL_RenderPresent(renderer);   // affichage
-    /*SDL_RenderClear(renderer);*/
-    /******************************************************/
-    printf("fin affichage\n");
-    SDL_Delay(5000);
+    init_map(map);
+    for (i = 0; i < 20; ++i)
+    {
+        x_perso += 1 ;
+        y_perso -= 1 +  (-1)*(i % 2);
+        placement_perso(map, x_perso, y_perso);
+        draw(renderer, map, couleurs); // appel de la fonction qui crée l'image
+        SDL_RenderPresent(renderer);   // affichage
+        SDL_RenderClear(renderer);
+        effacement_perso(map, x_perso, y_perso);
+        SDL_Delay(200);
+    } /******************************************************/
+
+    SDL_Delay(2000);
 
     end_sdl(0, "Fin normal", fenetre, renderer);
     return EXIT_SUCCESS;
 }
 
+void draw(SDL_Renderer *renderer, int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int couleurs[10][3])
+{
+    int i, j, c;
+    SDL_Rect rect;
+    for (i = 0; i < HAUTEUR_MATRICE; i++)
+    {
+        for (j = 0; j < LARGEUR_MATRICE; j++)
+        {
+            c = map[i][j];
+            SDL_SetRenderDrawColor(renderer, couleurs[c][0], couleurs[c][1], couleurs[c][2], 255);
+            rect.x = j * TAILLE_PIXEL;
+            rect.y = i * TAILLE_PIXEL;
+            rect.w = 1 * TAILLE_PIXEL;
+            rect.h = 1 * TAILLE_PIXEL;
+            SDL_RenderFillRect(renderer, &rect);
+        }
+    }
+}
+
+void init_map(int map[HAUTEUR_MATRICE][LARGEUR_MATRICE])
+{
+    int i, j;
+    for (i = 0; i < HAUTEUR_MATRICE; i++)
+        for (j = 0; j < LARGEUR_MATRICE; j++)
+            map[i][j] = 0;
+}
+
+void placement_perso(int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int x_perso, int y_perso)
+{
+    int i, j;
+    int x = x_perso;
+    int y = y_perso;
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            y = y_perso - i;
+            x = x_perso + j;
+            printf("x=%d; y=%d\n", x, y);
+            map[y][x] = dessine_perso(i, j);
+        }
+    }
+}
+void effacement_perso(int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int x_perso, int y_perso)
+{
+    int i, j;
+    int x = x_perso;
+    int y = y_perso;
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            y = y_perso - i;
+            x = x_perso + j;
+            map[y][x] = 0;
+        }
+    }
+}
+int dessine_perso(int i, int j)
+{
+    int partie_perso;
+    if (j >= 1 && j <= 2)
+    {
+        if (i >= 0 && i <= 3)
+        {
+            partie_perso = 1; //jambes
+        }
+        else if (i == 4)
+        {
+            partie_perso = 5; //ceinture
+        }
+        else if (i >= 5 && i <= 7)
+        {
+            partie_perso = 2; //t-shirt
+        }
+        else if (i == 8)
+        {
+            partie_perso = 3; //tete
+        }
+        else if (i == 9)
+        {
+            partie_perso = 4;
+        }
+    }
+    else if (j == 0 || j == 3)
+    {
+        if (i >= 4 && i <= 7)
+        {
+            partie_perso = 3; //bras
+        }
+        else
+        {
+            partie_perso = 0;
+        }
+    }
+    return partie_perso;
+}
+
+/*
+*
+*/
 void end_sdl(char ok,            // fin anormale : ok = 0 ; normale ok = 1
              char const *msg,    // message à afficher
              SDL_Window *window, // fenêtre à fermer
@@ -91,102 +197,4 @@ void end_sdl(char ok,            // fin anormale : ok = 0 ; normale ok = 1
     {
         exit(EXIT_FAILURE);
     }
-}
-
-void draw(SDL_Renderer *renderer, int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int couleurs[10][3])
-{
-    SDL_Rect rect;
-    int k, i, c;
-    for (k = 0; k < HAUTEUR_MATRICE; k++)
-    {
-        for (i = 0; i < LARGEUR_MATRICE; i++)
-        {
-            c = map[k][i];
-            printf("c=%d\n", c);
-            if (c >= 0 && c < 6)
-            {
-                SDL_SetRenderDrawColor(renderer, couleurs[c][0], couleurs[c][1], couleurs[c][2], 255);
-                rect.x = LARGEUR_PIXEL * k;
-                rect.y = HAUTEUR_PIXEL * i;
-                rect.w = LARGEUR_PIXEL;
-                rect.h = HAUTEUR_PIXEL;
-                if (rect.y >= 0 && rect.y < HAUTEUR_FENETRE)
-                    if (rect.x >= 0 && rect.x < LARGEUR_FENETRE)
-                        SDL_RenderFillRect(renderer, &rect);
-            }
-        }
-    }
-}
-
-void init_map(int map[HAUTEUR_MATRICE][LARGEUR_MATRICE])
-{
-    int i, j;
-
-    for (i = 0; i < HAUTEUR_MATRICE; i++)
-    {
-        for (j = 0; j < LARGEUR_MATRICE; j++)
-        {
-            map[i][j] = 0;
-        }
-    }
-}
-
-
-
-void placement_perso(int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int x_pos, int y_pos)
-{
-    int i, j;
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 4; j++)
-        {
-            y_pos = y_pos - i;
-            x_pos = x_pos + j;
-            if (y_pos >= 0 && y_pos < HAUTEUR_MATRICE)
-                if (x_pos >= 0 && x_pos < LARGEUR_MATRICE)
-                    map[x_pos][y_pos] = 5;
-        }
-    }
-}
-
-int dessine_perso(int i, int j)
-{
-    int partie_perso;
-    if (j >= 1 && j <= 2)
-    {
-        if (i >= 0 && i <= 2)
-        {
-            partie_perso = 1; //jambes
-        }
-        else if (i >= 4 && i <= 7)
-        {
-            partie_perso = 2; //t-shirt
-        }
-        else if (i == 8)
-        {
-            partie_perso = 3; //tete
-        }
-        else if (i == 9)
-        {
-            partie_perso = 4; //cheveux
-        }
-        else if (i == 3)
-        {
-            partie_perso = 5; //ceinture
-        }
-    }
-    else if (j == 3 || j == 0)
-    {
-        if (i >= 4 && i <= 7)
-        {
-            partie_perso = 1; //bras
-        }
-    }
-    else
-    {
-        partie_perso = 0; //ciel
-    }
-    if (j == 0 && i == 9)
-        partie_perso = 4; //casquette
-    return partie_perso;
 }
--- animation_simple/animation_simple.h
+++ animation_simple/animation_simple.h
@@ -4,20 +4,23 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-#define LARGEUR_FENETRE 1000
+#define LARGEUR_FENETRE 500
 #define HAUTEUR_FENETRE 500
 
-#define LARGEUR_MATRICE 300
-#define HAUTEUR_MATRICE 200
+#define LARGEUR_MATRICE 100
+#define HAUTEUR_MATRICE 100
+
+#define TAILLE_PIXEL 5
 
 #define HAUTEUR_PIXEL LARGEUR_FENETRE / HAUTEUR_MATRICE
 #define LARGEUR_PIXEL LARGEUR_FENETRE / LARGEUR_MATRICE
 
-void end_sdl(char ok, char const *msg, SDL_Window *window, SDL_Renderer *renderer);
 void init_map(int map[HAUTEUR_MATRICE][LARGEUR_MATRICE]);
-void draw(SDL_Renderer *renderer, int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int couleurs[10][3]);
 
-void placement_perso(int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int x_pos, int y_pos);
+void draw(SDL_Renderer *renderer, int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int couleurs[10][3]);
+void placement_perso(int map[HAUTEUR_MATRICE][LARGEUR_MATRICE], int x, int y);
 int dessine_perso(int i, int j);
+
+void end_sdl(char ok, char const *msg, SDL_Window *window, SDL_Renderer *renderer);
 
 #endif
