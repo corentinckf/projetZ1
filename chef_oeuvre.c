32f5c87c4be26df53ab0182f93e5c780755e704e
anthirion <antoine.thirion@etu.uca.fr>
2021-06-17 16:05:10 +0200

    affichage des items

--- chef_oeuvre.c
+++ chef_oeuvre.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -24,7 +25,7 @@ void init(int grille[LIGNES][COLONNES])
 
 void evolution(int grille[LIGNES][COLONNES],int *etape)
 {
-    int valeur = rand()%4 + 1;     //valeur entre 1 et 4
+    int valeur = rand()%5;     //valeur entre 0 et 4
     int signe = 2 * rand()%2 - 1; //signe vaut -1 ou 1
     int j;
 
@@ -34,30 +35,40 @@ void evolution(int grille[LIGNES][COLONNES],int *etape)
     }
 }
 
-void draw(SDL_Renderer* renderer, int grille[LIGNES][COLONNES], SDL_Texture *planche, int *hauteur_pixel, int *largeur_pixel)
+void draw(SDL_Renderer* renderer, int grille[LIGNES][COLONNES], SDL_Texture *planche, int *etape, int *hauteur_pixel, int *largeur_pixel)
 {
     SDL_Rect 
              source = {0},
              destination = {0},
              state = {0};
-    SDL_QueryTexture(planche,NULL, NULL,&source.w, &source.h);
+    
+    SDL_QueryTexture(planche,NULL, NULL, &source.w, &source.h);
 
     int offset_x = source.w / 4;
     int offset_y = source.h / 4;
     int i,j;
+    int ligne = 0;
 
-    float zoom = 1;
-    destination.w = source.w * zoom;
-    destination.h = source.h * zoom;
+    float zoom = 0.5;
+    destination.w = offset_x * zoom;
+    destination.h = offset_y * zoom;
+
+    state.w = offset_x;
+    state.h = offset_y;
     
     for (i=0;i<LIGNES;i++)
     {
+        //fprintf(stdout,"premier for\n");
+        ligne = (*etape + i) % LIGNES;
         destination.x = i * (*hauteur_pixel);
+
         for (j=0;j<COLONNES;j++)
         {
+            //fprintf(stdout,"deuxieme for\n");
             destination.y = j * (*largeur_pixel);
-            switch (grille[i][j])
+            switch (grille[ligne][j])
             {
+                //fprintf(stdout,"valeur : %d\n", grille[ligne][j]);
                 case 0:
                     //ne rien afficher : on voit le background
                     break;
@@ -97,13 +108,13 @@ void draw(SDL_Renderer* renderer, int grille[LIGNES][COLONNES], SDL_Texture *pla
                     fprintf(stdout,"valeur dans grille incoherente");
                     break;
             }
+            SDL_RenderCopy(renderer,planche,&state,&destination);  
+            SDL_RenderPresent(renderer);
+            SDL_Delay(10);
+            //SDL_SetRenderDrawColor(renderer,0,0,0,255);
+            SDL_RenderClear(renderer);
         }
-    }
-    SDL_RenderCopy(renderer,planche,&state,&destination);  
-    SDL_RenderPresent(renderer);
-    SDL_Delay(100);
-    SDL_SetRenderDrawColor(renderer,0,0,0,255);
-    SDL_RenderClear(renderer);
+    }   
 }
 
 int main()
@@ -116,32 +127,58 @@ int main()
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) 
     {
-        SDL_Log("Error : SDL initialisation - %s\n", SDL_GetError());      // l'initialisation de la SDL a échoué 
+        SDL_Log("Error : SDL initialisation - %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
     window = SDL_CreateWindow("Jeu de la vie",
                         SDL_WINDOWPOS_CENTERED,
-                        SDL_WINDOWPOS_CENTERED, 1000,
+                        SDL_WINDOWPOS_CENTERED, 600,
                         600,
                         SDL_WINDOW_OPENGL);
-    if (window == NULL) fprintf(stdout,"erreur d'ouverture de la fenetre");
+    if (window == NULL) fprintf(stderr,"erreur lors de l'ouverture de la fenêtre");
 
     renderer = SDL_CreateRenderer(
-            window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == NULL) fprintf(stdout,"erreur d'ouverture du renderer");
+                window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL) fprintf(stderr,"erreur lors de l'ouverture du rendu\n");
 
     int hauteur_pixel = 600 / LIGNES;
     int largeur_pixel = 1000 / COLONNES;
 
     SDL_Texture *planche = NULL;
 
-    init(grille);
-
-    for (int cpt; cpt < 1000; cpt++)
+    planche = IMG_LoadTexture(renderer,"./exemples_executables/img2/buttons.png");
+    if (planche == NULL) 
     {
-        draw(renderer,grille,planche,&hauteur_pixel,&largeur_pixel);
-        evolution(grille,&etape);
+        fprintf(stderr,"erreur lors de l'ouverture des boutons\n");
+    }
+
+    SDL_bool program_on = SDL_TRUE;
+
+    init(grille);
+    //fprintf(stdout,"grille initialisee\n");
+
+    while (program_on)
+    {
+        SDL_Event event;
+
+        while (program_on && SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT :
+                    program_on = SDL_FALSE;
+                    break;
+                default:
+                    break;
+            }
+        }
+        //fprintf(stdout,"on rentre dans la boucle\n");
+        draw(renderer,grille,planche,&etape,&hauteur_pixel,&largeur_pixel);
+        //fprintf(stdout,"dessin fait\n");
+        //evolution(grille,&etape);
         etape++;
     }
+    SDL_DestroyTexture(planche);
+    IMG_Quit();
 }
