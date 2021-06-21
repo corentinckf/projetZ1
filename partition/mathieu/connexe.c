b6b694cf47ef73162086011709753aff9c0c8d33
mathieu <ma_th21@hotmail.fr>
2021-06-21 18:20:48 +0200

    fin composante connexe

--- partition/mathieu/connexe.c
+++ partition/mathieu/connexe.c
@@ -4,6 +4,7 @@ int main()
 {
     srand(48);
 
+    /*
     int graph_mat[NB_LIGNE_MAT][NB_COLONNE_MAT];
     init_mat_alea(graph_mat);
 
@@ -13,9 +14,18 @@ int main()
     int part_connexe[2][N];
     int hauteur_part[N];
     creer_partition_arbo(part_connexe, hauteur_part);
-
     connexe_graph_mat(part_connexe, hauteur_part, graph_mat);
+    graphviz_affiche_arbo(part_connexe);
+*/
 
+    graph_couple_t *graph = init_graph_couple_alea();
+
+    graphviz_affiche_graph_couple(graph);
+
+    int part_connexe[2][N];
+    int hauteur_part[N];
+    creer_partition_arbo(part_connexe, hauteur_part);
+    connexe_graph_couple(part_connexe, hauteur_part, graph);
     graphviz_affiche_arbo(part_connexe);
 
     return 0;
@@ -93,9 +103,79 @@ void connexe_graph_mat(int part[2][N], int hauteur[N], int mat[N][N])
         {
             if (mat[i][j] == 1)
             {
-                printf("i=%d et j=%d\n", i, j);
+                //printf("i=%d et j=%d\n", i, j);
                 fusion_arbo(part, hauteur, i, j);
             }
         }
     }
+}
+
+graph_couple_t *init_graph_couple_alea()
+{
+    graph_couple_t *graph_couple = (graph_couple_t *)malloc(sizeof(graph_couple_t));
+    if (graph_couple)
+    {
+        graph_couple->nb_noeud = N;
+        graph_couple->nb_arete = 0;
+        couple_t *couple_tmp = malloc(sizeof(couple_t) * graph_couple->nb_noeud * graph_couple->nb_noeud / 2);
+        int v;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = i + 1; j < N; j++)
+            {
+                v = rand() % 15;
+                if (v == 1)
+                {
+                    couple_tmp[graph_couple->nb_arete].a = i;
+                    couple_tmp[graph_couple->nb_arete].b = j;
+                    graph_couple->nb_arete++;
+                }
+            }
+        }
+        graph_couple->liste_couple = (couple_t *)malloc(sizeof(couple_t) * graph_couple->nb_arete);
+        for (int k = 0; k < graph_couple->nb_arete; k++)
+        {
+            graph_couple->liste_couple[k].a = couple_tmp[k].a;
+            graph_couple->liste_couple[k].b = couple_tmp[k].b;
+        }
+        free(couple_tmp);
+    }
+    return graph_couple;
+}
+
+void connexe_graph_couple(int part_connexe[2][N], int hauteur_part[N], graph_couple_t *graph)
+{
+    for (int i = 0; i < graph->nb_arete; i++)
+    {
+        fusion_arbo(part_connexe, hauteur_part, graph->liste_couple[i].a, graph->liste_couple[i].b);
+    }
+}
+
+void fichier_graphviz_graph_couple(graph_couple_t *graph)
+{
+    FILE *fichier = NULL;
+    //system("rm graph.dot");
+    fichier = fopen("graph.dot", "w");
+    if (fichier == NULL)
+        exit(EXIT_FAILURE);
+
+    fprintf(fichier, "graph { ");
+    for (int i = 0; i < graph->nb_arete; i++)
+    {
+        //printf("i=%d\n", i);
+        fprintf(fichier, "\n\t%d--%d", graph->liste_couple[i].a, graph->liste_couple[i].b);
+    }
+    for (int i = 0; i < graph->nb_noeud; i++)
+        fprintf(fichier, "\n\t%d", i);
+
+    fprintf(fichier, "\n} ");
+
+    fclose(fichier);
+}
+
+void graphviz_affiche_graph_couple(graph_couple_t *graph)
+{
+    fichier_graphviz_graph_couple(graph);
+    system("dot -Tjpg graph.dot -o img.jpg");
+    system("display ./img.jpg 2> /dev/null");
 }
--- partition/mathieu/connexe.h
+++ partition/mathieu/connexe.h
@@ -6,6 +6,19 @@
 
 #include "partition.h"
 
+typedef struct couple
+{
+    int a;
+    int b;
+} couple_t;
+
+typedef struct graph_couple
+{
+    int nb_noeud;
+    int nb_arete;
+    couple_t *liste_couple;
+} graph_couple_t;
+
 #define N 20
 #define NB_LIGNE_MAT N
 #define NB_COLONNE_MAT N
@@ -13,10 +26,15 @@
 void afficher_matrice(int mat[NB_LIGNE_MAT][NB_COLONNE_MAT]);
 
 void init_mat_alea(int mat[NB_LIGNE_MAT][NB_COLONNE_MAT]);
+graph_couple_t *init_graph_couple_alea();
 
 void fichier_graphviz_graph_mat(int mat[NB_LIGNE_MAT][NB_COLONNE_MAT]);
 void graphviz_affiche_graph_mat(int mat[NB_LIGNE_MAT][NB_COLONNE_MAT]);
 
 void connexe_graph_mat(int part[2][N], int hauteur[N], int mat[N][N]);
+void connexe_graph_couple(int part_connexe[2][N], int hauteur_part[N], graph_couple_t *graph);
+
+void fichier_graphviz_graph_couple(graph_couple_t *graph);
+void graphviz_affiche_graph_couple(graph_couple_t *graph);
 
 #endif
