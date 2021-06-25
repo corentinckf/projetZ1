29bc4be94a5fb322e9e11c00d9adf760cdb9706c
Rouxeyz <85785335+Rouxeyz@users.noreply.github.com>
2021-06-25 16:09:49 +0200

    Update ui.h

--- labyboule/ui.h
+++ labyboule/ui.h
@@ -5,4 +5,6 @@
 
 char *texte_score(int a);
 void affichage_texte(SDL_Window *window, SDL_Renderer *renderer, TTF_Font *font, char *texte, float taille, int x, int y);
+void ecran_fin(SDL_Window *window, SDL_Renderer *renderer, TTF_Font *font, int coll);
+
 #endif
